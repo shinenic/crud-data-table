2ab55015338e66ba5d7fbad2a6f07e20402bad13
ENTIN WEI <galadiya41@gmail.com>
2019-10-27 11:55:55 +0800

    Updates

---
+++ precache-manifest.009cf77f31c733c68a0d50a3e9251bd2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7362e26631320c00662a92451d22d583",
+    "url": "/crud-data-table/index.html"
+  },
+  {
+    "revision": "55d8926acc11d9c54816",
+    "url": "/crud-data-table/static/css/main.73fd2c80.chunk.css"
+  },
+  {
+    "revision": "4b73dcba335f8273cab7",
+    "url": "/crud-data-table/static/js/2.e6170c3a.chunk.js"
+  },
+  {
+    "revision": "55d8926acc11d9c54816",
+    "url": "/crud-data-table/static/js/main.5e4d13bf.chunk.js"
+  },
+  {
+    "revision": "ac6ad1f36eedb8250d78",
+    "url": "/crud-data-table/static/js/runtime-main.68f9bf91.js"
+  },
+  {
+    "revision": "c514d2137f79a24f854ca195b052bf6c",
+    "url": "/crud-data-table/static/media/check.c514d213.svg"
+  },
+  {
+    "revision": "fbc4bf146967ad3c197a9e999a0fa369",
+    "url": "/crud-data-table/static/media/delete.fbc4bf14.svg"
+  },
+  {
+    "revision": "d37d94008347a67f103811601107af7f",
+    "url": "/crud-data-table/static/media/edit.d37d9400.svg"
+  },
+  {
+    "revision": "79a8908cfeef724471fba8f2bb0c7598",
+    "url": "/crud-data-table/static/media/recover.79a8908c.svg"
+  },
+  {
+    "revision": "c953591cb71fb82d8282a09355ba7ba4",
+    "url": "/crud-data-table/static/media/sort.c953591c.svg"
+  },
+  {
+    "revision": "fb0a12a456983659462dbf65f5612d26",
+    "url": "/crud-data-table/static/media/sorted.fb0a12a4.svg"
+  }
+]);
